9eb4105879e2a14efc3d16eb0fa5bd4e0a9c1262
TruEnot <vadiccrut@gmail.com>
2020-12-14 20:44:00 +0400

    complete

--- bot/main.py
+++ bot/main.py
@@ -2,8 +2,8 @@ import telebot
 import json
 from work import *
 import datetime
-from openpyxl import Workbook, load_workbook
 import os
+
 
 config = json.load(open('config.json'))
 
@@ -13,14 +13,42 @@ bot = telebot.TeleBot(config['token'])
 def new_message(message):
 
     data = find_data(message.text)
-    xls_path = str(message.chat.id) + '.xls'
+    xlsx_path = str(message.chat.id) + '.xlsx'
+    date = datetime.date.today()
 
-    bot.send_message(message.chat.id, data.__str__())
+    #bot.send_message(message.chat.id, data.__str__())
+    if data != None:
 
-    if not os.path.isfile(xls_path):
-    	wb = Workbook()
-    	wb.save(xls_path)
+        if not os.path.isfile(xlsx_path):
+            wb = openpyxl.Workbook()
+            ws = wb[wb.sheetnames[0]]
+            ws['a1'].value = 'Дата'
+            ws['b1'].value = 'Название'
+            ws['c1'].value = '-'
+            ws['d1'].value = '+'
+            wb.save(xlsx_path)
 
-    wb = load_workbook(xls_path)
+        wb = openpyxl.load_workbook(xlsx_path)
+        ws = wb[wb.sheetnames[0]]
+
+        ws.cell(ws.max_row + 1, 1).value = '{0}.{1}.{2}'.format(date.day, date.month, date.year)
+        ws.cell(ws.max_row, 2).value = data[0]
+        if data[1] < 0:
+            ws.cell(ws.max_row, 3).value = abs(data[1])
+        elif data[1] > 0:
+            ws.cell(ws.max_row, 4).value = abs(data[1])
+
+        wb.save(xlsx_path)
+
+        bot.send_message(message.chat.id, 'OK')
+
+    elif message.text.lower() in config['triggers']:
+        f = open(xlsx_path, "rb")
+        p = open(create_photo(xlsx_path), 'rb')
+        bot.send_document(message.chat.id, f)
+        bot.send_photo(message.chat.id, p)
+
+        os.system('rm temp.png')
+
 
 bot.polling(none_stop=True, interval=0)
--- bot/work.py
+++ bot/work.py
@@ -1,3 +1,6 @@
+from PIL import Image, ImageDraw, ImageFont
+import openpyxl
+
 def can_float(num):
     try:
         float(num.replace(',', '.'))
@@ -24,6 +27,66 @@ def find_data(text):
                 num = -num
 
         else:
-            txt += word
+            txt += ' ' + word
 
-    return [txt, num] if num and text != '' else None
+
+    if txt != '' and num:
+        return [txt, num]
+
+    else:
+        return None
+
+
+def create_photo(document_name):
+    wb = openpyxl.load_workbook(document_name)
+    ws = wb[wb.sheetnames[0]]
+
+    img = Image.new('RGB', (500,1000 if ws.max_row > 50 else 20 * ws.max_row+20),(255,255,255))
+    font = ImageFont.truetype('Roboto-Black.ttf', size=13)
+    draw = ImageDraw.Draw(img)
+
+
+    draw.rectangle((0, 0, 500, 1000), (255, 255, 255), (187, 187, 187))
+    for x in range(50):
+        draw.line((0, (x+1)*20, 500, (x+1)*20), (187, 187, 187), 1)
+
+    for x in range(4):
+        draw.line((x*125, 0, x*125, 1000),(187, 187, 187),1)
+
+    row = 0
+
+    sum = 0
+    pls = 0
+    mns = 0
+
+    for x in ws:
+        if x[0].row < ws.max_row - 50:continue
+
+        date = x[0].value
+        comment = x[1].value
+        m = x[2].value
+        p = x[3].value
+
+        if date != 'Дата':
+            pls += p if p else 0
+            mns += m if m else 0
+
+        d = 0
+        for x in [date, comment, m, p]:
+            if x == None:x = ''
+            draw.text((125 * d + 5, 20 * row+6), str(x), fill=(0,0,0),font=font)
+
+            d += 1
+
+        row += 1
+
+    sum = pls - mns
+
+    draw.text((125*1,20*row+1),str(sum),fill=(0,0,0),font = font)
+    draw.text((125 * 2 + 5, 20 * row + 1), str(mns), fill=(0, 0, 0), font=font)
+    draw.text((125 * 3 + 5, 20 * row + 1), str(pls), fill=(0, 0, 0), font=font)
+
+    img.save('temp.png')
+
+    return 'temp.png'
+
